e6ddb11cdaf8fad6fa6dc6f5a691f7b0eb665965
wuzh <276229118@qq.com>
2017-03-26 12:13:49 +0800

    Fix runtime permission; Update gradle, support library

--- app/src/main/java/cafe/adriel/androidaudiorecorder/example/MainActivity.java
+++ app/src/main/java/cafe/adriel/androidaudiorecorder/example/MainActivity.java
@@ -10,6 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
 import cafe.adriel.androidaudiorecorder.AndroidAudioRecorder;
 import cafe.adriel.androidaudiorecorder.model.AudioChannel;
 import cafe.adriel.androidaudiorecorder.model.AudioSampleRate;
@@ -19,19 +27,33 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_RECORD_AUDIO = 0;
     private static final String AUDIO_FILE_PATH =
             Environment.getExternalStorageDirectory().getPath() + "/recorded_audio.wav";
+    private boolean hasPermission = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Dexter.initialize(this.getApplicationContext());
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setBackgroundDrawable(
                     new ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimaryDark)));
         }
 
-        Util.requestPermission(this, Manifest.permission.RECORD_AUDIO);
-        Util.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Dexter.checkPermissions(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()){
+                    hasPermission = true;
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
 
     @Override
@@ -47,6 +69,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void recordAudio(View v) {
+        if (! hasPermission){
+            Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+            return;
+        }
         AndroidAudioRecorder.with(this)
                 // Required
                 .setFilePath(AUDIO_FILE_PATH)
